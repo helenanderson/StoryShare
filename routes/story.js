0265037f2186b751175199bb4a73c2ebdcd7d38a
Sam Gussman <sgussman@stanford.edu>
2014-02-09 13:34:28 -0800

    Making changes to dynamically create the story page.

--- routes/story.js
+++ routes/story.js
@@ -1,6 +1,8 @@
 var data = require( '../storyData.json');
 
 exports.view = function(req, res){
+	console.log(req.params.title);
 	console.log(data);
-	res.render("StorPage", data);
+	res.render("mainpage", data);
+	// res.render("StorPage", data);
 };
