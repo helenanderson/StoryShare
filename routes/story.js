5e17353a21d14c9f5f1582308aaa2a87a053da45
vagrant <vagrant@precise32.(none)>
2014-02-10 04:31:11 +0000

    I forgot to have the story.js

--- routes/story.js
+++ routes/story.js
@@ -1,14 +1,8 @@
 var data = require( '../storyData.json');
 
 exports.view = function(req, res){
-	// console.log(req.params.title);
-	// console.log(data);
 	var name = req.params.title;
-	//var storyobj = data.stories
 	console.log(name);
 	console.log(data.stories[name]);
-	// console.log(data.name);
-	//console.log(req.params);
-	//res.render("mainpage", data.stories.req.params.title);
-	 res.render("StoryPage", data);
+	res.render("StoryPage", data.stories[name]);
 };
