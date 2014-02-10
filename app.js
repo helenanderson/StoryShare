92afb7c92282d8f430816e7e80da34d3a1dd10fa
vagrant <vagrant@precise32.(none)>
2014-02-10 04:48:29 +0000

    Made it so that the page dynamically generates the story! I also fixed the linking with the formatting pages.

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(express.cookieParser('Intro HCI secret key'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
