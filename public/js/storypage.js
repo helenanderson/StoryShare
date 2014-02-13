d9927f91a85359e9bf4b7dd0a310d01049df70bc
vagrant <vagrant@precise32.(none)>
2014-02-13 00:59:17 +0000

    I have made a bunch of changes. First I figured out if the 'add to story' button should appear.

---
+++ public/js/storypage.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+	console.log("Javascript connected!");
+// 	var story = $("#story1").text();
+// 	//var numSentences = $(this.".sentences");
+// 	//if (numSentences == 10) console.log("The Story is complete.");
+// 	//else if (numSentences == 9) console.log("The Story has " + numSentences + "sentences.");
+// 	console.log(story);
+// }
+}
--- routes/story.js
+++ routes/story.js
@@ -3,6 +3,8 @@ var data = require( '../storyData.json');
 exports.view = function(req, res){
 	var name = req.params.title;
 	console.log(name);
-	console.log(data.stories[name]);
-	res.render("StoryPage", data.stories[name]);
+	var storyData = data.stories[name];
+	console.log(storyData);
+	storyData._finished = storyData.sentences == 10;
+	res.render("StoryPage", storyData);
 };
