00890b5b174b787abe1d4912a0d0e0e6df50ead1
vagrant <vagrant@precise32.(none)>
2014-02-13 04:24:39 +0000

    I made it so that the 'addToStory' button changes to a text box when the user clicks it. I did realize that I made it so that the 'addToStory' button only appears on the stories that are already finished, so I'm going to go back and fix that, then re-commit

--- public/js/storypage.js
+++ public/js/storypage.js
@@ -7,10 +7,17 @@ $(document).ready(function() {
 
 function initializePage() {
 	console.log("Javascript connected!");
+	$('#addBtn').click(addToStory);
 // 	var story = $("#story1").text();
 // 	//var numSentences = $(this.".sentences");
 // 	//if (numSentences == 10) console.log("The Story is complete.");
 // 	//else if (numSentences == 9) console.log("The Story has " + numSentences + "sentences.");
 // 	console.log(story);
 // }
+}
+
+function addToStory(e) {
+	console.log("AddToStory button clicked.");
+	$(this).replaceWith("<div class='row'><div class='col-lg-6'><div class='input-group'><span class='input-group-btn'><button class='btn btn-default' type='button'>Go!</button></span><input type='text' class='form-control'></div></div><div class='col-lg-6'><div class='input-group'><input type='text' class='form-control'><span class='input-group-btn'><button class='btn btn-default' type='button'>Add!</button></span></div></div></div>");
+
 }
