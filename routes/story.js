1de0a1ff7809735dbad59ac7d9cfb85550558a3a
Sam Gussman <sgussman@stanford.edu>
2014-02-19 21:15:16 -0800

    The stories now link to themselves and generate data pulled from the database when they are clicked on.

--- routes/story.js
+++ routes/story.js
@@ -1,12 +1,25 @@
 var data = require( '../storyData.json');
-var model = require('../models.js');
+var models = require('../models.js');
 
 exports.view = function(req, res){
-	var name = req.params.title;
-	var storyData = data.stories[name];
-	//console.log(storyData);
-	storyData._finished = storyData.sentences < 10;
-	res.render("StoryPage", storyData);
+	var storyID = req.params.id;
+	console.log(storyID);
+	models.Story
+		.find({"_id": storyID})
+		.exec(afterStoryQuery)
+	
+
+	function afterStoryQuery(err, stories) {
+		if(err) console.log(err);
+		console.log(stories[0]);
+		// var story = JSON.stringify(stories[0]);
+		var story = stories[0];
+		story["finished"]=story.sentences < 10;
+		console.log(story.finished);
+		res.render("StoryPage", story);
+	}
+	// storyData._finished = storyData.sentences < 10;
+	// res.render("StoryPage", Story);
 };
 
 exports.add = function (req, res){
