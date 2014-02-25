14917ecb2b115eacd7e7397658c7c4754e2bface
vagrant <vagrant@precise32.(none)>
2014-02-25 02:22:37 +0000

    made about page the landing page

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
-app.get('/', main.view);
+app.get('/main', main.view);
+app.get('/', about.view);
 app.get('/story/:id', story.view);//This displays the individual stories after clicking their thumbnail.
 app.get('/help', help.view);
 //maggie added next line
