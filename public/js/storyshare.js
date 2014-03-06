ff481d7b0b21a5be48df662b925d27a52134a63c
vagrant <vagrant@precise32.(none)>
2014-03-06 00:57:47 +0000

    GA changes

--- public/js/storyshare.js
+++ public/js/storyshare.js
@@ -6,23 +6,57 @@ $(document).ready(function() {
 })
 
 function initializePage() {
-	ga("send", "event", "StartWritingButton", "click");
-	ga("send", "event", "HelpLandingBtn", "click");
-	ga("send", "event", "AddToAStoryBtn", "click");
-	ga("send", "event", "CreateANewStoryBtn", "click");
-	ga("send", "event", "BookIconBtn", "click");
-	ga("send", "event", "PencilIconBtn", "click");
-	ga("send", "event", "PlusIconBtn", "click");
-	ga("send", "event", "MenuBtn", "click");
-	ga("send", "event", "AboutMenuBtn", "click");
-	ga("send", "event", "HelpMenuBtn", "click");
-	ga("send", "event", "AddToStoryMenuBtn", "click");
-	ga("send", "event", "ArchiveMenuBtn", "click");
-	ga("send", "event", "NewStoryMenuBtn", "click");
+	$("#startWriting").click(startWritingClick);
+	$("#addToStory").click(addToStoryClick);
+	$("#startNewStory").click(startNewStoryClick);
+	$("#helpLanding").click(helpLandingClick);
+	$("#bookIcon").click(bookIconClick);
+	$("#pencilIcon").click(pencilIconClick);
+	$("#plusIcon").click(plusIconClick);
+	
+	
+	
+	
+	
+	
+	
+	//ga("send", "event", "MenuBtn", "click");
+	//ga("send", "event", "AboutMenuBtn", "click");
+	//ga("send", "event", "HelpMenuBtn", "click");
+	//ga("send", "event", "AddToStoryMenuBtn", "click");
+	//ga("send", "event", "ArchiveMenuBtn", "click");
+	//ga("send", "event", "NewStoryMenuBtn", "click");
 	//$('#startWritingBtn').click(startWriting);
 	console.log("Javascript connected!");
 }
 
+function startWritingClick(e) {
+	ga("send", "event", "StartWritingButton", "click");
+}
+
+function addToStoryClick(e) {
+	ga("send", "event", "AddToStoryBtn", "click");
+}
+
+function startNewStoryClick(e) {
+	ga("send", "event", "StartNewStoryBtn", "click");
+}
+
+function helpLandingClick(e) {
+	ga("send", "event", "helpLandingBtn", "click");
+}
+
+function bookIconClick(e) {
+	ga("send", "event", "bookIconBtn", "click");
+}
+
+function pencilIconClick(e) {
+	ga("send", "event", "pencilIconBtn", "click");
+}
+
+function plusIconClick(e) {
+	ga("send", "event", "plusIconBtn", "click");
+}
 
 /*function StartWriting(e) {
 		ga("send", "event", "button", "click");
