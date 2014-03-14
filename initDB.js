5478902b7702541ac89dfb86f67a78996e52e1b1
Sam Gussman <sgussman@stanford.edu>
2014-03-14 12:06:26 -0700

    removing peniss

--- initDB.js
+++ initDB.js
@@ -30,7 +30,7 @@ var projects_json = require('./stories.json');
 // Step 2: Remove all existing documents
 models.Story
 //find the ones that have an empty title.
-  .find({"title" : "Final Design Days"})
+  .find({"title" : "Baby swap"})
   .remove()
   .exec(onceClear); // callback to continue at
 
