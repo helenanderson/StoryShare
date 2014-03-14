419aeb9c3ff0a2e67d5242cef66f6662b2747d04
Sam Gussman <sgussman@stanford.edu>
2014-03-14 11:55:28 -0700

    I'm removing the stories that say penis.

--- initDB.js
+++ initDB.js
@@ -30,7 +30,7 @@ var projects_json = require('./stories.json');
 // Step 2: Remove all existing documents
 models.Story
 //find the ones that have an empty title.
-  .find({"title" : ""})
+  .find({"title" : "The Camera Told Me So"})
   .remove()
   .exec(onceClear); // callback to continue at
 
