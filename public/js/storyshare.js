3aece66df44a98727a3457890a042cff2b71dccf
vagrant <vagrant@precise32.(none)>
2014-03-19 19:02:22 +0000

    really deleted ga code

--- public/js/storyshare.js
+++ public/js/storyshare.js
@@ -10,32 +10,7 @@ function initializePage() {
 console.log("page initialized!");
 
 
-	$("#startWriting").click(startWritingClick);
-	$("#addToStory").click(addToStoryClick);
-	$("#startNewStory").click(startNewStoryClick);
-	$("#helpLanding").click(helpLandingClick);
+	
 	
 }
 
-
-
-function mainLink() {
-    window.location.replace("/main");
-}
-
-
-
-
-function addLink() {
-    window.location.replace("/new");
-}
-
-
-function helpLandingClick(e) {
-    e.preventDefault();
-    ga('send', 'event', 'helpLandingBtn', 'click', { 'hitCallback':helpLink } );
-}
-
-function helpLink() {
-    window.location.replace("/help");
-}
