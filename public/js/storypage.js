f2c2fe23ea41ff60c2dca970737f01eb6978d5ca
vagrant <vagrant@precise32.(none)>
2014-03-20 09:04:43 +0000

    edited landing page text

--- public/js/storypage.js
+++ public/js/storypage.js
@@ -30,7 +30,7 @@ function addToStoryInit(e) {
 	console.log("AddToStory button clicked.");
 	var ID = window.location.pathname.match(/\/story\/(.*)/)[1];
 	console.log(ID);
-	$(this).replaceWith("<form id ='textToAddForm' method ='post' action = '/story/update/"+ID+"'><div class='form-group'><textarea name = 'text' id='textToAdd' style='width: 100%' rows='8' cols='40' maxlength='150' placeholder='What happens next?'></textarea><span class='count'></span></div><button id = 'submitBtn' class='btn start-button' style = 'margin-top: 10px' type='submit'><h3>Add !</h3></button><button id = 'cancelBtn' class='btn start-button' style = 'margin-top: 10px' value= 'click' onClick='goBack()' type='button'><h3>Cancel</h3></button></form><div><span>");
+	$(this).replaceWith("<form id ='textToAddForm' method ='post' action = '/story/update/"+ID+"'><div class='form-group'><textarea name = 'text' id='textToAdd' style='width: 100%' rows='5' cols='40' maxlength='150' placeholder='What happens next?'></textarea><span class='count'></span></div><button id = 'submitBtn' class='btn start-button' style = 'margin-top: 10px' type='submit'><h3>Add !</h3></button><button id = 'cancelBtn' class='btn start-button' style = 'margin-top: 10px' value= 'click' onClick='goBack()' type='button'><h3>Cancel</h3></button></form>");
 	//The next two lines remove the back button because it is replaced by a cancel  
 	//button (see line above) that links to a different page
 	var backBtn = document.getElementById('backBtn');
@@ -39,15 +39,17 @@ function addToStoryInit(e) {
 	$('#textToAddForm').submit(addToStory);
 
 
-	$(function() {
+	
+	
+}
+
+$(function() {
 	    $("#textToAdd").characterCounter({
 			counterCssClass: 'count',
 			limit: 150,
 			counterFormat: '%1 characters remaining.'
 		});
  	});
-	
-}
 
 //When Cancel button is clicked, form disappears (returns to Story page)
 function goBack(e) {
